61e000309e95fe0878921438c66b4e1ff089d917
oohm099 <71598868+oohm099@users.noreply.github.com>
2020-09-20 21:46:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ __**Command list**__
 > \`search\` > **\`search [title]\`**
 > \`skip\`, \`stop\`,  \`pause\`, \`resume\`
 > \`nowplaying\`, \`queue\`, \`volume\``)
-            .setFooter("©️ 2020 Zhycorp Nation", "https://api.zhycorp.xyz/assets/images/icon.jpg");
+            .setFooter("©️ 2090 Khunphan Jairak", "https://api.zhycorp.xyz/assets/images/icon.jpg");
         message.channel.send(helpembed);
     }
     if (command === "play" || command === "p") {
